dfdf976ad45cecdb213b903007d7e6c75a667811
vikramjha293@gmail.com <vikram@bloomsmobility.com>
2018-09-13 23:12:38 +0530

    ....

---
+++ app/src/main/java/com/example/abhinav_rapidbox/childdaycare/activity/LocationActivity.java
@@ -0,0 +1,14 @@
+package com.example.abhinav_rapidbox.childdaycare.activity;
+
+import android.os.Bundle;
+
+import com.example.abhinav_rapidbox.childdaycare.R;
+
+public class LocationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
